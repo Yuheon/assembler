917dedcd88b4b9e037c9becb4f6cfe6daffc4cc6
Yuheon <syh0466@naver.com>
2018-11-12 22:09:19 +0900

    4th

--- trans.c
+++ trans.c
@@ -18,9 +18,29 @@ int instr_trans(char *op, char *args, char* mcode)
 		return 0;
 	i++;
 	
-	if(args[0]=='%'&&args[i]=='%')
-		mcode=89;
-
+	if(args[0]=='%'&&args[i]=='%')//reg to reg
+		mcode="89";
+	else if(args[0]=='('||args[0]=='-'){//mem to reg case 2,3
+		if(args[i]=='%')
+			mcode="8b";
+	}
+	else if(args[0]=='0'&&args[i]=='%')//mem to reg case 4
+		mcode="a1";
+	else if(args[0]=='%'&&(args[i]=='0'||args[i]=='('))//eax to mem
+		mcode="a3";
+	else if(args[0]=='$'&&args[i]=='%')//imd to reg
+	{
+		if(args[i+2]=='a')
+			mcode="b8";
+		else if(args[i+2]=='b')
+			mcode="b9";
+		else if(args[i+2]=='c')
+			mcode="ba";
+		else if(args[i+2])
+			mcode="bb";
+	}
+	else
+		return 0;
 	/********************************/
     /*  							*/
 	/*    generate machine code     */
