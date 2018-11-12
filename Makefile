30276ba7addcaa5f82fd17f7c3fba9944e35405c
Yuheon <syh0466@naver.com>
2018-11-12 20:44:04 +0900

    2nd

--- Makefile
+++ Makefile
@@ -1,2 +1,13 @@
 # Write this Makefile
+myas: main.o check.o trans.o
+	gcc -o myas main.o check.o trans.o -lm
+main.o: main.c
+	gcc -O -Wall -c main.c
+check.o: check.c
+	gcc -O -Wall -c check.c
+trans.o: trans.c
+	gcc -O -Wall -c trans.c
+clean:
+	rm -f main.o check.o trans.o myas
+
 
--- check.c
+++ check.c
@@ -2,6 +2,22 @@
 
 int is_valid(char *op, char *args)
 {
+int i;
+for(i=0;args[i]!=NULL;i++){
+                if(args[i]==',')
+                        break;
+};
+if(args[i]==NULL)
+	return 0;
+i++;
+if(args[0]=='('||args[0]=='0'||args[0]=='-'){
+	
+	 if(args[i]=='('||args[i]=='0'){
+		return 0;
+}
+
+if(args[i]=='$')
+	return 0;
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
 
